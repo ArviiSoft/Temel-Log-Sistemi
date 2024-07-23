655b002b74f254136217dc7e04fb40f7c1764b6b
ArviS <alkannefee@gmail.com>
2024-07-23 14:29:07 +0330

    Add files via upload

---
+++ başlat.bat
@@ -0,0 +1,8 @@
+@echo off
+color 04
+title arvis. - Temel Log Sistemi
+cls
+:arvis
+node arvis.js
+goto arvis
+pause
---
+++ test.bat
@@ -0,0 +1,7 @@
+@echo off
+color 04
+title arvis. - Temel Log Sistemi TEST
+cls
+:arvis2
+npm run arvis:dev
+goto arvis2
